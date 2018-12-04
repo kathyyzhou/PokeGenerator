59cfa1945cc1fef29bc702cff280a106b4adbeea
Kathy Zhou <kathy2@illinois.edu>
2018-12-04 15:36:01 -0600

    moved api call to pokemon.java

--- app/src/main/java/com/example/kathyzhou/pokegenerator/pokemon.java
+++ app/src/main/java/com/example/kathyzhou/pokegenerator/pokemon.java
@@ -8,8 +8,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 
 public class Pokemon extends AppCompatActivity {
+
+    /** Request queue for our network requests. */
+    private static RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,8 @@ public class Pokemon extends AppCompatActivity {
         final Button regenerate = findViewById(R.id.regenerate);
         final Button home = findViewById(R.id.home);
 
+        // Set up a queue for our Volley requests
+        requestQueue = Volley.newRequestQueue(this);
 
         home.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -37,6 +51,38 @@ public class Pokemon extends AppCompatActivity {
                 }
             }
         });
+
+        Log.d("API", "pokemon but before API call");
+        startAPICall();
+        Log.d("API", "Started API on Pokemon.java");
+
+    }
+
+    /**
+     * Make an API call.
+     */
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://pokeapi.co/api/v2/",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d("API", response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w("API", error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
