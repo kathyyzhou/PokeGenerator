bc5e6419fa465da1d694823a01fcc74d0e6fe761
Kathy Zhou <kathy2@illinois.edu>
2018-12-04 14:59:43 -0600

    regenerate button now logs a message

--- app/src/main/java/com/example/kathyzhou/pokegenerator/pokemon.java
+++ app/src/main/java/com/example/kathyzhou/pokegenerator/pokemon.java
@@ -1,5 +1,6 @@
 package com.example.kathyzhou.pokegenerator;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -18,18 +19,24 @@ public class Pokemon extends AppCompatActivity {
         final Button regenerate = findViewById(R.id.regenerate);
         final Button home = findViewById(R.id.home);
 
+
         home.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (v  == home) {
                     Intent intentMain = new Intent(Pokemon.this, MainActivity.class);
                     Pokemon.this.startActivity(intentMain);
                     Log.d("button", "Pokemon to Main Layout");
-                } else if (v == regenerate) {
-                    Log.d("button", "New Pokemon Button clicked");
                 }
             }
         });
 
+        regenerate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (v == regenerate) {
+                    Log.d("button", "regenerate a new pokemon");
+                }
+            }
+        });
     }
 
 }
