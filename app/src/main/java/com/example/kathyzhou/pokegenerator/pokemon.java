c91826d38105d66b881e52de20de1a669ace34aa
Kathy Zhou <kathy2@illinois.edu>
2018-12-08 21:50:27 -0600

    cleaned up some unused imports

--- app/src/main/java/com/example/kathyzhou/pokegenerator/pokemon.java
+++ app/src/main/java/com/example/kathyzhou/pokegenerator/pokemon.java
@@ -1,9 +1,6 @@
 package com.example.kathyzhou.pokegenerator;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -17,7 +14,6 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.JsonArray;
@@ -25,11 +21,6 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.Random;
 
